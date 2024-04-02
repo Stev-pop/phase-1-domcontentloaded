fd6ce40c8be27507c5a480045cf8bf6f8ea26fe9
Stephen Onyango <onyangosteve01@gmail.com>
2024-04-03 02:48:56 +0300

    Adds assignment to github

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+  } );
+  
+  
+  function updateDOM() {
+    let paragraphText = document.getElementById("text")
+    paragraphText.textContent = "This is really cool!";
+  }
